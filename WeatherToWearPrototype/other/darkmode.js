0081fb49a136a9646c2f7b4ad49fe14115fbb334
JaminBucur <jcbucur@uncg.edu>
2024-04-18 11:50:43 -0400

    Fixed Dark Mode

--- WeatherToWearPrototype/other/darkmode.js
+++ WeatherToWearPrototype/other/darkmode.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
             document.body.classList.toggle("dark-mode");
             localStorage.setItem("darkModeEnabled", document.body.classList.contains("dark-mode"));
         });
+
+        if (localStorage.getItem("darkModeEnabled") === "true") {
+            document.body.classList.add("dark-mode");
+        }
     } else {
         console.error("Element with ID 'dark-mode-toggle' not found.");
     }
