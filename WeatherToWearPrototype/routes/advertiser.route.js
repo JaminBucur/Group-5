36f8702ad96a5667117632b1bf3a80e923eccea1
Arturo D. <40877732+delaA2018@users.noreply.github.com>
2024-04-29 20:45:10 -0400

    "added conditional statement incase image was not changed"

--- WeatherToWearPrototype/routes/advertiser.route.js
+++ WeatherToWearPrototype/routes/advertiser.route.js
@@ -18,6 +18,40 @@ module.exports = function (imageUpload) {
     router.get("/getAllAds", advertiserController.getAllAds);
     
     router.post("/createDiscount", advertiserController.createDiscount);
+
+    // creating function for /createDiscount where it handles the case if the user does not change the image
+    router.post("/createDiscount", upload.single("Image"), (req,res) => {
+        let newImage = req.file;
+        let adId = req.body.adId;
+    
+        if (newImage) {
+            // new image is provided
+            let sql = 'UPDATE Advertisement SET Image = ? WHERE AdID = ?';
+            let params = [newImage.filename, adId];
+    
+            db.run(sql, params, function(err){
+                if (err) {
+                    console.error(err);
+                    res.status(500).send({ message: 'Failed to update ad.' });
+                } else {
+                    res.status(200).send({ message: 'Ad updated successfully.' });
+                }
+            });
+        } else {
+            // no new image was provided, should keep old image
+            let sql = 'UPDATE Advertisement SET ClothingName = ?, HeatIndex = ?, Price = ? WHERE AdID = ?';
+            let params = [req.body.ClothingName, req.body.HeatIndex, req.body.Price, adId];
+    
+            db.run(sql, params, function(err){
+                if (err) {
+                    console.error(err);
+                    res.status(500).send({ message: 'Failed to update ad.' });
+                } else {
+                    res.status(200).send({ message: 'Ad updated successfully.' });
+                }
+            });
+        }
+    });
     
     router.get("/getDiscountSales/:id", advertiserController.getDiscountSaleById);
     /*
